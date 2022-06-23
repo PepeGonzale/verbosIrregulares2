c1ae358c16fc130e5e98d7b2aa2dff3871f0d7c1
Pepe Gonzalez <pepeglezdevega@gmail.com>
2022-06-23 17:31:35 +0200

    Estrellas puestas

--- js/script.js
+++ js/script.js
@@ -7,26 +7,43 @@ const boton3 = document.getElementById('boton3')
 const botonResultado = document.getElementById('btn-resultado')
 const resultado = document.querySelector('.aciertos')
 const pantallaInicio = document.querySelector('.principal')
+const estrellas = document.querySelector('.estrellas')
 const nivel = document.getElementById('inicio')
 const niveles = document.querySelector('.niveles')
+const numeroEstrellas = 16
+const relleno = "bi bi-star"
 
 let verboRandom = 0;
 let tiempoAdivinar= 0
 let acierto = 0;
 let nive = 0
+let icono = ''
 
+function metodoPrincipal (){
+    creaEstrellas()
+    eligeVerbo()
+}
 function eligeNivel(id){
     nive = parseInt(id, 10)
-    niveles.innerHTML = nive
+    niveles.innerHTML = "El nivel es: " + nive
 }
 function chequeaVerbo(){
     let verboLeido = document.getElementById('caja').value
   
     if(verbos[verboRandom][tiempoAdivinar] === verboLeido){
+        
         eligeVerbo()
         acierto++
     }
     resultado.innerHTML = acierto
+}
+function creaEstrellas(){
+    for (var i = 0; i < numeroEstrellas; i++){        
+        icono += '<i class = "bi bi-star" id = "' + i + '"></i>'      
+        estrellas.innerHTML = icono
+              
+    }
+    
 }
 function eligeVerbo(){
     
@@ -60,7 +77,7 @@ botonResultado.addEventListener('click', () => {
     chequeaVerbo()
 })
 console.log(verbos[verboRandom][tiempoAdivinar])
-eligeVerbo()
+metodoPrincipal()
 
 
 
